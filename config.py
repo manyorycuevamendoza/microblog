c1c442990c35a4d6aeec57c4d76fdc3a1c4d8944
Manyory Cueva Mendoza <91238621+manyorycuevamendoza@users.noreply.github.com>
2022-08-25 18:21:42 -0500

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://postgres:UTEC@localhost:5432/postgres'
+        'postgresql://manyorycueva:76591212@localhost:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
